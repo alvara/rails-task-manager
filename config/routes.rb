b3868ab6c7aa93f5110b965f7c6e22f2f7efe14d
Jesse Alvarado <contact@jessealvarado.com>
2021-11-09 14:38:48 +0900

    implement delete and update and edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # READ ALL
+  # Read all tasks
   get '/tasks', to: 'tasks#index', as: :tasks
 
-  # READ 1
+  # Create New Task
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  post '/tasks', to: 'tasks#create', as: :create_task
+
+  # read individual task
   get '/tasks/:id', to: 'tasks#show', as: :task
+
+  # edit individual task
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update', as: :update_task
+
+  # delete individual task
+  delete '/tasks/:id', to: 'tasks#destroy', as: :delete_task
 end
